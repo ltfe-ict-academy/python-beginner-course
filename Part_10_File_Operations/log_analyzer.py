22f0289eb91e7dc79f48de0a3840a501539e74b1
Leon <leon@ltfe.org>
2025-12-08 19:56:52 +0100

    add day 09

---
+++ Part_10_File_Operations/log_analyzer.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+log_file_path = Path(__file__).parent / "server.txt"
+
+
+def read_logs(path: Path) -> list[str]:
+    """Read all logs into a list of strings."""
+    with open(path, encoding="utf-8") as log_file:
+        all_lines = log_file.readlines()
+    return [line.strip() for line in all_lines if line.strip()]
+
+
+def show_logs_level_summary(lines: list[str]) -> None:
+    """Show logs level summary."""
+
+
+def main() -> None:
+    """Run logs analysis."""
+    print(f"Starting analysis for file: {log_file_path}")
+    all_lines = read_logs(log_file_path)
+    print(f"Total lines: {len(all_lines)}")
+    show_logs_level_summary(all_lines)
+
+
+if __name__ == "__main__":
+    main()
